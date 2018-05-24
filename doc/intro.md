e2ee49b4549de7d3c05224291a721c83784eb2ca
gvvvv <gvvvv@163.com>
2018-05-24 09:55:15 +0800

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to algorithm-data-structure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/algorithm_data_structure/core_test.clj
@@ -0,0 +1,7 @@
+(ns algorithm-data-structure.core-test
+  (:require [clojure.test :refer :all]
+            [algorithm-data-structure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
